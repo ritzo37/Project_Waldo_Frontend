2d63cb0b2a7897c4176a56e0244cfb3830bc3e23
ritzo37 <ritishrai56@gmail.com>
2025-10-18 13:33:20 +0530

    fix login

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -4,7 +4,7 @@ const logInUrl = import.meta.env.VITE_BASEURL + "/log-in";
 export default function LogIn() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState([]);
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   useEffect(() => {
     if (localStorage.getItem("token")) {
@@ -23,8 +23,8 @@ export default function LogIn() {
       credentials: "include",
     });
     const data = await response.json();
-    if (data.errors) {
-      setErrors(data.errors);
+    if (response.status !== 200) {
+      setError(data.message);
     } else {
       const token = data.token;
       localStorage.setItem("token", JSON.stringify(token));
@@ -33,13 +33,7 @@ export default function LogIn() {
   }
   return (
     <>
-      {errors.length > 0 && (
-        <ul>
-          {errors.map((currError) => {
-            return <li>{currError.msg}</li>;
-          })}
-        </ul>
-      )}
+      {error.length > 0 && <p>{error}</p>}
       <form onSubmit={handleFormSubmit}>
         <label htmlFor="username">Username</label>
         <input
